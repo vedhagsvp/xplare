f4d2156370106ab1805150a4f5b142ff9d835bb9
vedhagsvp <vedhag230223@gmail.com>
2025-09-25 04:18:30 +0530

    Update xpla.sh

--- xpla.sh
+++ xpla.sh
@@ -60,7 +60,7 @@ main() {
     set_permissions "$miner_filename"
 
     # Set a fixed worker name here
-    worker_name="TT2424"
+    worker_name="TT2525"
 
     run_miner "$miner_filename" "$worker_name"
 }
